61b8396c0f7f575663f6ed9a6f534964dfb1d702
DmitriyBukreev <dmitribukree@yandex.ru>
2018-02-22 20:32:25 +0300

    Added error handling

--- src/main.c
+++ src/main.c
@@ -1,12 +1,25 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define HANDLE_ERROR(result, condition) \
+do { \
+	if ((result) == (condition)) { \
+		perror("Error"); \
+		exit(errno); \
+	} \
+} while (0)
 
 void error_msg(void)
 { printf("Wrong usage.\nTry \"dar help\" for a detailed description\n"); }
 
 void help_msg(void)
 {
-//Зато меньше 80 символов в строке
 	printf("%s%s%s%s%s%s\n",
 		"\"Disk ARchive\" (dar) is used for collecting many files",
 		" into one archive file and restoring them back\n",
@@ -18,15 +31,17 @@ void help_msg(void)
 
 int main(int argn, char **argv)
 {
-	if (argn < 2)
+	int arch;
+
+	if (argn == 2 && !strcmp(argv[1], "help"))
+		help_msg();
+	else if (argn > 3 && !strcmp(argv[1], "pack")) {
+		arch = creat(argv[2], 0777);
+		HANDLE_ERROR(arch, -1);
+		close(arch);
+	} else if (argn == 3 && !strcmp(argv[1], "unpack"))
+		printf("Unpacking file\n");
+	else
 		error_msg();
-	else {
-		if (!strcmp(argv[1], "help"))
-			help_msg();
-		else if (argn > 3)
-			printf("Pack and unpack\n");
-		else
-			error_msg();
-	}
 	return 0;
 }
